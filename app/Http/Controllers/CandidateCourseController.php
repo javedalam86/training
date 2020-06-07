c6c8f6411748d6d2eec4f23131d5d70fada425bc
Javed Alam <javedalam86@gmail.com>
2020-06-07 05:16:45 +0000

    Updated quize result entry

--- app/Http/Controllers/CandidateCourseController.php
+++ app/Http/Controllers/CandidateCourseController.php
@@ -5,21 +5,21 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request; 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
-use App\Models\Pages; 
-//use App\Models\MyCourses; 
-use App\Models\CandidateCourses; 
+use App\Models\Pages;
+//use App\Models\MyCourses;
+use App\Models\CandidateCourses;
 use App\Imports\UsersImport;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\Models\Courses;
-use App\Models\Books; 
-use App\Models\CourseQuize; 
+use App\Models\Books;
+use App\Models\CourseQuize;
 
 class CandidateCourseController extends Controller
 {
-	
+
 	var $recordPerPage =20;
     /**
      * Display a listing of the resource.
@@ -28,68 +28,68 @@ class CandidateCourseController extends Controller
      */
     public function candidatecourselist()
     {
-       // $pages = Pages::all();	
+       // $pages = Pages::all();
         $Courses = CandidateCourses::where('is_deleted', '=', '0')->orderBy('id', 'ASC' )->get();
-	
+
         return view('candidatecourselist', compact('Courses'));
     }
-	
+
 	public function candidatecoursedetail(Request $request, $id)
     {
         $CourseData = courses::where('is_deleted', '=', '0')->where('id', '=', $id)->get()->toArray();
 		$Course =  $CourseData[0];
-		
-		$BookData = Books::select('course_books.id as id','courses.name as cname','course_books.description','type','course_books.name','course_id','bookpath');		  
+
+		$BookData = Books::select('course_books.id as id','courses.name as cname','course_books.description','type','course_books.name','course_id','bookpath');
 		$BookData->leftJoin('courses', function ($join) {
             $join->on('courses.id', '=', 'course_books.course_id');
 			});
 		$BookData->where('course_books.course_id', '=', $id);
-		$BookData =	$BookData->get()->toArray();	
-	
+		$BookData =	$BookData->get()->toArray();
+
 		$CourseQuizeData = CourseQuize::where('course_quize_status', '=', '1')->where('course_id', '=', $id)->get()->toArray();
-		
-		
-		
-		
+
+
+
+
         return view('candidatecoursedetail', compact('Course','BookData','CourseQuizeData'));
     }
-	
-	
+
+
 	public function candidatetest(Request $request)
     {
         $MyCourseData = User::where('is_deleted', '=', '0')->get()->toArray();
 		$MyCourse =  $MyCourseData[0];
         return view('candidatetest', compact('MyCourse'));
     }
-	
+
 	function ajaxcandidatecourselist(Request $request){
 		$data = $request->all();
-		$page =$data['pagination']['page'];	
-		$per_page = $data['pagination']['perpage'];	
+		$page =$data['pagination']['page'];
+		$per_page = $data['pagination']['perpage'];
 		if($per_page ==''){
-		$per_page =10; }		
+		$per_page =10; }
 		$method='GET';
 		$searchArray['per_page']=$per_page;
 		$searchArray['page']	=$page;
 		$candidateId = Auth::user()->id;
-	
-		$CourseData = CandidateCourses::select('courses.id as id','courses.name as cname','courses.description as description');	
-	  
+
+		$CourseData = CandidateCourses::select('courses.id as id','courses.name as cname','courses.description as description');
+
 		$CourseData->leftJoin('courses', function ($join) {
             $join->on('courses.id', '=', 'candidate_courses.course_id');
 		});
 		$CourseData->where('candidate_courses.candidate_id', '=', $candidateId);
-		//$CourseRawData =	$CourseData->get()->toArray();		
+		//$CourseRawData =	$CourseData->get()->toArray();
 
 		//$candidateData =  CandidateCourses::where('is_deleted', '=', '0')->orderBy('id', 'ASC' )->get();
-		//$candidateData =  CandidateCourses::where('is_deleted', '=', '0')->orderBy('id', 'ASC' )->get();		
+		//$candidateData =  CandidateCourses::where('is_deleted', '=', '0')->orderBy('id', 'ASC' )->get();
 
 		if(@isset($data['query']['coursegeneralSearch'])){
-			$searchKey =$data['query']['coursegeneralSearch'];	
+			$searchKey =$data['query']['coursegeneralSearch'];
 			$CourseData = $CourseData->where(function($q) use ($searchKey){
 				$q->where('name', 'LIKE', '%' . $searchKey . '%');
 		});
-		}	
+		}
 		$sortfield ='id';
 		if(@isset($data['sort']['field']) ){
 			if(in_array($data['sort']['field'], array('course','id'))){
@@ -105,36 +105,36 @@ class CandidateCourseController extends Controller
 		$CourseData->offset($offset);
         $CourseData->limit($this->recordPerPage);
 		$CourseData->orderBy($sortfield, $sortorder);
-		$CourseData =	$CourseData->get()->toArray();		
-		
-		
+		$CourseData =	$CourseData->get()->toArray();
+
+
 		//$userCount = CandidateCourses::select('*');
-		$AllCourseData = CandidateCourses::select('courses.id as id','courses.name as cname','courses.description as description');		  
+		$AllCourseData = CandidateCourses::select('courses.id as id','courses.name as cname','courses.description as description');
 		$AllCourseData->leftJoin('courses', function ($join) {
             $join->on('courses.id', '=', 'candidate_courses.course_id');
 		});
 		$AllCourseData->where('candidate_courses.candidate_id', '=', $candidateId);
-		
-		
+
+
 		if(@isset($data['query']['coursegeneralSearch'])){
-			$searchKey =$data['query']['coursegeneralSearch'];	
+			$searchKey =$data['query']['coursegeneralSearch'];
 			$AllCourseData = $AllCourseData->where(function($q) use ($searchKey){
 				$q->where('name', 'LIKE', '%' . $searchKey . '%');
 		});
 		}
 		$AllCourseData->where('candidate_courses.is_deleted', '=', '0');
-		$AllCourseData =$AllCourseData->get();			
-		$totalRecord =$AllCourseData->count();	
-		
-		$resultDataTable['data'] = $CourseData;	
+		$AllCourseData =$AllCourseData->get();
+		$totalRecord =$AllCourseData->count();
+
+		$resultDataTable['data'] = $CourseData;
 		$resultDataTable['meta']['page'] = $page;
 		$resultDataTable['meta']['pages'] = ceil($totalRecord/$per_page);
 		$resultDataTable['meta']['perpage'] = $per_page;
 		$resultDataTable['meta']['total'] = $totalRecord;
 			return json_encode($resultDataTable);
 }
-	
-	
+
+
 
 
 /**
@@ -145,20 +145,20 @@ class CandidateCourseController extends Controller
      */
     public function mycourseadd(Request $request)
     {		$data = $request->all();
-			$validator = Validator::make($request->all(), [ 
-				'name'=> 'required|min:5', 
-				
-			]);					
-		if ($validator->fails()) { 
+			$validator = Validator::make($request->all(), [
+				'name'=> 'required|min:5',
+
+			]);
+		if ($validator->fails()) {
 			return response(array("status"=>"fail", "code"=>400,'message' => $validator->errors(),"data" => $data));
 		}else{
-			$input['name'] =$data['name'];				
+			$input['name'] =$data['name'];
 			$input['description'] =$data['description'];
-						
-			$course = MyCourses::create($input);		
+
+			$course = MyCourses::create($input);
 			return response(array("status"=>"success", "code"=>200,"data" => $data));
-		}	
-       
+		}
+
     }
 
 
@@ -170,22 +170,22 @@ class CandidateCourseController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function editmycourse(Request $request)
-    {     
+    {
 		$data = $request->all();
-		$validator = Validator::make($request->all(), [ 
-				'courseId' => 'required', 
-				'name' => 'required|min:5', 
-				
-			]);					
-		if ($validator->fails()) { 
+		$validator = Validator::make($request->all(), [
+				'courseId' => 'required',
+				'name' => 'required|min:5',
+
+			]);
+		if ($validator->fails()) {
 			return response(array("status"=>"fail", "code"=>400,'message' => $validator->errors(),"data" => $data));
 		}else{
 			$id = $data['courseId'];
 
-        $Course = MyCourses::find($id);		
+        $Course = MyCourses::find($id);
         $Course->name 		=  $request->get('name');
-        $Course->description 		=  $request->get('description');		
-        
+        $Course->description 		=  $request->get('description');
+
         $Course->save();
       return response(array("status"=>"success", "code"=>200,"data" => $Course));
     }}
@@ -199,8 +199,8 @@ class CandidateCourseController extends Controller
 
 public function parseImport(Request $request)
 {	$data = $request->all();
-    $file     = request()->file('file');	 
-	$path = $file->getPathname();	
+    $file     = request()->file('file');
+	$path = $file->getPathname();
 	$CSVdata = array_map('str_getcsv', file($path));
 	$alreadyExistingQuestion = array();
 	$duplicateCourses =0;
@@ -210,34 +210,34 @@ public function parseImport(Request $request)
 		array_shift($CSVdata);
 	}
 
-	foreach($CSVdata as $CSVRow){	
+	foreach($CSVdata as $CSVRow){
 			$course = $CSVRow['0'];
-			$correctOption =$CSVRow['5'];		
+			$correctOption =$CSVRow['5'];
 			if(strlen($correctOption)>1){
 				$optionLength =1;
 			}
 		$QuestionRecord = MyCourses::where('is_deleted', '=', '0')->where('course', '=', $course)->get()->toArray();
 		if(count($QuestionRecord)>=1){
 			$alreadyExistingQuestion[]= $course;
-			$duplicateCourses =1;	
+			$duplicateCourses =1;
 		}
 	}
 	if($duplicateCourses ==1){
 		return response(array("status"=>"fail", "type"=>'duplicate', "code"=>200,"data" => $alreadyExistingQuestion));
 	}elseif($optionLength ==1){
 		return response(array("status"=>"fail", "type"=>'answerlength', "code"=>200,"data" => $alreadyExistingQuestion));
-	}else{	
-		
+	}else{
+
 		foreach($CSVdata as $CSVRow){
-			$input = array();			
-			$input['course'] =$CSVRow['0'];				
+			$input = array();
+			$input['course'] =$CSVRow['0'];
 			$input['option_a'] =$CSVRow['1'];
 			$input['option_b'] =$CSVRow['2'];
 			$input['option_c'] =$CSVRow['3'];
 			$input['option_d'] =$CSVRow['4'];
-			$input['correct_option'] =$CSVRow['5'];					
-			$course = MyCourses::create($input);			
-		}		
+			$input['correct_option'] =$CSVRow['5'];
+			$course = MyCourses::create($input);
+		}
 		return response(array("status"=>"success", "code"=>200,"data" => $course));
 	}
 }
@@ -247,17 +247,17 @@ public function parseImport(Request $request)
 
 
 public function deletemycourse(Request $request)
-{	$data = $request->all(); 
+{	$data = $request->all();
 	$id = $data['courseIdDelete'];
-	$Question = MyCourses::find($id);		
+	$Question = MyCourses::find($id);
     $Question->is_deleted 		=  1;
     $Question->save();
-    return response(array("status"=>"success", "code"=>200,"data" => $Question));	  
+    return response(array("status"=>"success", "code"=>200,"data" => $Question));
 }
 
 
 
 
-	
-	
+
+
 }
--- resources/views/onlinequize.blade.php
+++ resources/views/onlinequize.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.admin') 
+@extends('layouts.admin')
 @section('content')
 @php $scriptVer =  Config::get('constants.SCRIPT_VERSION'); @endphp
-@php $ROOT_PATH =  Config::get('constants.ROOT_PATH'); @endphp
+@php $ROOT_PATH =  Config::get('constants.ROOT_PATH');
+ @endphp
 <!-- end::Head -->
 <!-- begin::Body -->
  <meta name="_token" content="{{ csrf_token() }}" />
@@ -12,17 +13,17 @@
 		<!-- end:: Header Mobile -->
 		<div class="kt-grid kt-grid--hor kt-grid--root">
 			<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
-				@include('layouts.sidebar')	
+				@include('layouts.sidebar')
 				<!-- end:: Aside -->
 				<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper" id="kt_wrapper">
-				@include('layouts.adminheader')				
+				@include('layouts.adminheader')
 				<!-- end:: Header -->
-				<div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">	
+				<div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
 					<!-- begin:: Content Head -->
 						<div class="kt-subheader  kt-grid__item" id="kt_subheader">
 							<div class="kt-container  kt-container--fluid ">
 								<div class="kt-subheader__main">
-									<h3 class="kt-subheader__title"><i class="kt-font-brand flaticon-layer"></i> &nbsp; Quiz:</h3> 
+									<h3 class="kt-subheader__title"><i class="kt-font-brand flaticon-layer"></i> &nbsp; Quiz:</h3>
 									<!--	<span class="kt-subheader__separator kt-subheader__separator--v"></span>	-->
 									<div class="kt-input-icon kt-input-icon--right kt-subheader__search kt-hidden">
 										<input type="text" class="form-control" placeholder="Search order..." id="generalSearch">
@@ -32,9 +33,9 @@
 									</div>
 								</div>
 								<div class="kt-subheader__toolbar">
-									<div class="kt-subheader__wrapper">										 
-										 <button type="button" class="btn btn-brand btn-icon-sm" ><a href="{{ URL::previous() }}" style="color:white;"> <i class="flaticon2-plus"></i> Go Back</a>                                       
-                                    </button>									
+									<div class="kt-subheader__wrapper">
+										 <button type="button" class="btn btn-brand btn-icon-sm" ><a href="{{ URL::previous() }}" style="color:white;"> <i class="flaticon2-plus"></i> Go Back</a>
+                                    </button>
 									</div>
 								</div>
 							</div>
@@ -45,13 +46,47 @@
 
 						<!-- begin:: Content -->
 						<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
-							<div class="row">
+							@if ($message = Session::get('success'))
+                  <div class="col-md-12">
+                    <div class="alert alert-success in">
+                      <div class="col-md-11">
+                        <i class="fa fa-check faa-pulse animated"></i>
+                        <strong>Success: </strong>
+                        {{ $message }}
+                      </div>
+                       <div class="col-md-1">
+                        <button type="button" class="close pull-right" data-dismiss="alert">&times;</button>
+                       </div>
+                    </div>
+                  </div>
+               @endif
+
+              @if ($message = Session::get('error'))
+                <div class="col-md-12">
+                  <div class="alert alert alert-danger in">
+                    <div class="col-md-11">
+                      <i class="fa fa-exclamation-circle faa-pulse animated"></i>
+                     <strong>Error: </strong>
+                     {{ $message }}
+                    </div>
+                     <div class="col-md-1">
+                      <button type="button" class="close pull-right" data-dismiss="alert">&times;</button>
+                     </div>
+                  </div>
+                </div>
+              @endif
+              <div class="row">
 								<div class="col-lg-12">
 									<div class="kt-portlet portlet-fit full-height-content full-height-content-scrollable bordered">
 										<div class="col-lg-12 kt-portlet__head">
-											<div class="col-lg-12 kt-portlet__head-label">								
+											<div class="col-lg-12 kt-portlet__head-label">
 												<div class=" col-lg-12 kt-align-center">
-													<button type="reset" id="startQuizBtn" class="btn-lg btn-danger">Start Quize</button>
+                          @if(!empty($CourseQuize))
+                            <button type="reset" id="startQuizBtn" class="btn-lg btn-danger">Start Quize</button>
+                          @else
+                              In valid Quize
+                          @endif
+
 												</div>
 											</div>
 										</div>
@@ -61,16 +96,16 @@
 											 <li>Other Quiz instructions willl come here.</li>
 											 <li>Other Quiz instructions willl come here.</li>
 										  </ul>
-										</div>									
-									</div>	
-									
+										</div>
+									</div>
+
 								</div>
-							</div>	
-									
-						
-						
+							</div>
+
+
+
 							<div class="row" >
-								<div class="col-lg-12" id="quizeQuestionContainer" style="display:none">		
+								<div class="col-lg-12" id="quizeQuestionContainer" style="display:none">
 									<!--begin::Portlet-->
 									<div class="kt-portlet portlet-fit full-height-content full-height-content-scrollable bordered">
 										<div class="kt-portlet__head">
@@ -81,36 +116,31 @@
 											</div>
 										</div>
 
-										<!--begin::Form
-										<form class="kt-form kt-form--label-right">-->
+										<!--begin::Form -->
+										<form class="kt-form kt-form--label-right" method="POST" id="candidateQuizeForm" action="{{route('candidate-submit-quize')}}">
+                        {{ csrf_field() }}
 											<div class="kt-portlet__body" id="quizeQuestionsDiv">
-											
-												
 											</div>
-											
-											
-											
-											<div class="kt-portlet__foot">
+                      <div class="kt-portlet__foot">
 												<div class="kt-form__actions">
 													<div class="row">
 														<div class="col-lg-6">
-														
+
 															<button type="button" onclick="showPreviousQuestion()" class="btn btn-secondary">Privious Question</button>
 																<button type="button" onclick="showNextQuestion()" class="btn btn-primary">Next Question</button>
 														</div>
 														<div class="col-lg-6 kt-align-right">
-															<button type="reset" class="btn btn-danger">Submit Quize</button>
+															<button type="submit" class="btn btn-danger">Submit Quize</button>
 														</div>
 													</div>
 												</div>
 											</div>
-										<!--</form>
-
-										end::Form-->
+										</form>
+										<!-- end::Form-->
 									</div>
 
 									<!--end::Portlet-->
-								</div>												
+								</div>
 							</div>
                 </div>
    <!-- end:: Content -->
@@ -119,20 +149,20 @@
 
 
 
-               <!-- begin:: Content 
+               <!-- begin:: Content
                 <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
 				<div id='ajaxmessagediv'></div>
                     <div class="kt-portlet kt-portlet--mobile">
-                       
-                        <div class="kt-portlet__body">                         
-                    	<div class="kt-portlet__body">						
-						Quize will come here..					
-												
-						</div>											
-												
-                            
+
+                        <div class="kt-portlet__body">
+                    	<div class="kt-portlet__body">
+						Quize will come here..
+
+						</div>
+
+
                         </div>
-												
+
                     </div>
                 </div>
 -->
@@ -148,7 +178,7 @@
     <!-- end:: Page -->
     @include('layouts.adminotherpanels')
 
-   
+
 
 
     <!-- begin::Global Config(global config for global JS sciprts) -->
@@ -177,51 +207,75 @@
     <script src="{{ asset('assetsadmin/plugins/global/plugins.bundle.js?v='.$scriptVer) }}" type="text/javascript"></script>
 
     <script src="{{ asset('assetsadmin/js/scripts.bundle.js?v='.$scriptVer) }}" type="text/javascript"></script>
-	
+
 	<script src="{{ asset('assetsadmin/js/pages/dashboard.js?v='.$scriptVer) }}" type="text/javascript"></script>
-	
+
 	<script src="{{ asset('assetsadmin/js/pages/custom/user/profile.js?v='.$scriptVer) }}" type="text/javascript"></script>
 	<script type="text/javascript">
 	setTimeout(function() {
   $('#ajaxmessagediv').fadeOut('fast');
-}, 1200); 
+}, 1200);
 var ROOT_PATH = '{{$ROOT_PATH}}';
 var QuizeId = '{{$QuizeId}}';
 
 var currentQuestionId =0;
 var totalQuestions=0;
-	$('body').on('click',"#startQuizBtn",function(){	
+	$('body').on('click',"#startQuizBtn",function(){
  currentQuestionId =0;
- totalQuestions=0;	
+ totalQuestions=0;
 		/*$.ajaxSetup({
 			headers: {
 				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 			}
-		});	*/	
+		});	*/
 		var courseId = 31;
 		$.ajax({
 			type: "POST",
 			url: ROOT_PATH+"/ajaxgetquizequestion",
 			 data: {
                     "_token": "{{ csrf_token() }}", QuizeId:QuizeId},
-			success: function(msg){		
+			success: function(msg){
 				if(msg.status=='fail'){
 					/*$.each( msg.casedata.errors, function( key, value) {
 						for(var l=0;l<value.length; l++){
 							var fieldErrorId =  'error_'+editloanid+'_'+key;
-							
-							fieldErrorId = fieldErrorId.replace('.','');							
-							$("#"+fieldErrorId).text(value);						
+
+							fieldErrorId = fieldErrorId.replace('.','');
+							$("#"+fieldErrorId).text(value);
 						}
 					});
-						*/  
+						*/
 				}else{
-				
-					var questionsList = '';	
+				  $("#startQuizBtn").hide();
+					var questionsList = '';
+          var course_id;
 					$.each( msg.data, function( key, value) { totalQuestions++;
-					var question_id = "question_"+key;			
-					var option_name = "option_"+(key+1);			
-					questionsList +='<div id='+question_id+' class="questionsList" style="display:none">\
+					course_id = value.course_id;
+          var question_id = "question_"+key;
+					var option_name = "option_"+(key+1);
+          var question_type_name = "question_type_"+(key+1);
+          var question_id_name = "question_id_"+(key+1);
+          var question_section_id_name = "question_section_id_"+(key+1);
+          questionsList +='<input type="hidden" name="'+question_type_name+'"  value="'+value.question_type+'" >';
+          questionsList +='<input type="hidden" name="'+question_id_name+'"  value="'+value.id+'" >';
+          questionsList +='<input type="hidden" name="'+question_section_id_name+'"  value="'+value.section_id+'" >';
+          if(value.question_type == '1') {
+              questionsList +='<div id='+question_id+' class="questionsList" style="display:none">\
+                    <div class="form-group row">\
+                      <div class="col-lg-12">\
+                        <label>'+value.question+'</label>\
+                      </div>\
+                    </div>\
+                    <div class="form-group row">\
+                      <div class="col-lg-12">\
+                        <div class="kt-radio-inline">\
+                            <textarea class="form-control" rows="5" name="'+option_name+'" id="'+option_name+'"></textarea>\
+                        </div>\
+                      </div>\
+                    </div>\
+                  </div>';
+          } else {
+					   questionsList +='<div id='+question_id+' class="questionsList" style="display:none">\
 										<div class="form-group row">\
 											<div class="col-lg-12">\
 												<label>'+value.question+'</label>\
@@ -260,8 +314,12 @@ var totalQuestions=0;
 											</div>\
 										</div>\
 									</div>';
-					
-					}); 
+          }
+
+					});
+          questionsList +='<input type="hidden" name="totalQuestions"  value="'+totalQuestions+'" >';
+          questionsList +='<input type="hidden" name="quize_id"  value="'+QuizeId+'" >';
+          questionsList +='<input type="hidden" name="course_id"  value="'+course_id+'" >';
 $("#quizeQuestionsDiv").html(questionsList);
 $("#question_0").show();
 $("#quizeQuestionContainer").show();
@@ -270,40 +328,40 @@ setquestioninfo();
 					/*
 					removeblur(editloanid);
 				caseDataObject.casedata = msg.casedata.data;
-				loadLoanstable();	
-				
-				
-				alert("Success..")				
+				loadLoanstable();
+
+
+				alert("Success..")
 				setLoanCalcData(editloanid); toReloadTheLoanViewDivContent(editloanid);  */
-				} 
+				}
 				//$("#edit-loan-spinner_"+editloanid).hide();
 			}
-		}); 		
+		});
 	});
-function showNextQuestion(){  
+function showNextQuestion(){
 
 if(currentQuestionId >=(totalQuestions-1)){ return true; }
-		currentQuestionId++; 
+		currentQuestionId++;
 	$(".questionsList").hide();
-	$("#question_"+currentQuestionId).show();	
+	$("#question_"+currentQuestionId).show();
 	setquestioninfo();
 }
 function showPreviousQuestion(){
 	if(currentQuestionId ==0){ return true; }
-	currentQuestionId--;	
+	currentQuestionId--;
 	$(".questionsList").hide();
 	$("#question_"+currentQuestionId).show();
-	setquestioninfo();	
+	setquestioninfo();
 }
 function setquestioninfo(){
 	var QuestionInfoHTML ='Question No '+(currentQuestionId+1)+' of '+totalQuestions+':';
-	
+
 	$("#questionInfo").html(QuestionInfoHTML);
 }
 
 $(document).on("change", '.quetsionoptions', function(event){
-	
-   // var ElementId 		= $(this).attr("id"); 
+
+   // var ElementId 		= $(this).attr("id");
 	var ElementName 	= $(this).attr("name");
 	var Elementvalue 	= $(this).attr("value");
 	alert(ElementName+'---'+Elementvalue);
@@ -314,11 +372,12 @@ $(document).on("change", '.quetsionoptions', function(event){
     <!--begin::Page Vendors(used by this page)
    <script src="{{ asset('assets/plugins/custom/datatables/datatables.bundle.js') }}" type="text/javascript"></script> -->
     <!--end::Page Vendors -->
-    <!--begin::Page Scripts(used by this page) 
+    <!--begin::Page Scripts(used by this page)
    <script src="{{ asset('assets/js/pages/crud/datatables/data-sources/ajax-server-side1.js') }}" type="text/javascript"></script>-->
 
 
-   
+
+
    </body>
 <!-- end::Body -->
 @endsection
